eb7281dc9583e5703d0e30b5406ea393c640c3c0
李科学 <science168@126.com>
2016-08-19 10:25:35 +0800

    Initial Commit

---
+++ pushDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  pushDemo
+//
+//  Created by likexue on 16/8/19.
+//  Copyright © 2016年 likexue. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
